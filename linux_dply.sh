8fea172ac31d7efc8220d9b96db582a05f69bdcf
deseretsecops <113214308+deseretsecops@users.noreply.github.com>
2022-10-25 16:26:16 -0600

    Updates

--- linux_dply.sh
+++ linux_dply.sh
@@ -1,90 +1,17 @@
-#!/bin/bash
-
-echo 'We are going to check for git.'
-if ! command -v git &> /dev/null
-then
-    echo "... Ok, git could not be found"
-	 echo "************************"
-	  echo "Now installing git"
-	sudo apt install git
-    exit
-fi
-
-echo -e "\n \nChecking Files and Permission. \n \n"
-
-
-git clone https://github.com/deseretsecops/splunkdply.git
-
-if [ -d "splunkdply/" ] 
-	then
-		echo "hey git worked, changing directory to start installer...." 
-		echo "" 
-		echo "" 
-	else
-		echo "Error: Directory ..splunkdply/ missing ."
-		echo "Download error" >&2 
-		exit 1
-fi
-
-echo -e "\n \nDone, now setting file permissions.\n"
-
-chmod +x /helpers/*
-
-
-source ./helpers/install.sh
-
-BC=$'\e[4m'
-EC=$'\e[0m'
-
-printf "\n"
-read -p "Enter a deployment type (${BC}UF${EC} for universal forwarder or ${BC}ENT${EC} for Enterprise version) followed by enter> " deployVar
-
-deployVar=`printf "${deployVar}" | tr '[a-z]' '[A-Z]'`
-
- if [ $deployVar = "UF" ]; then
-
- 	if (( $(ps -ef | grep -v grep | grep splunkd | wc -l) != 0 )); then
-
-    		printf "\n"
-    		read -p "A splunk instance is already running, do you want to remove it? (y or n): " conf
-    		conf=`printf "${conf}" | tr '[A-Z]' '[a-z]'`
-
-    		if [ $conf = 'y' ]; then
-    				splunkRemove $deployVar
-    				splunkInstall $deployVar
-    		else
-    				printf "\nExiting....\n"
-    		fi
-
-  else
-    		splunkInstall $deployVar
-
-	 fi
-
- elif [ $deployVar = "ENT" ]; then
-
-
- 	 	if (( $(ps -ef | grep -v grep | grep splunkd | wc -l) != 0 )); then
-
-    		printf "\n"
-    		read -p "A splunk instance is already running, do you want to remove it? (y or n): " conf
-    		conf=`printf "${conf}" | tr '[A-Z]' '[a-z]'`
-
-    		if [ $conf = 'y' ]; then
-
-    				splunkRemove $deployVar
-    				splunkInstall $deployVar
-
-    		else
-    				printf "\nExiting....\n"
-    		fi
-
- 		 else
-
-    		splunkInstall $deployVar
-
-    fi
-
- else
-    printf "\nEnter a valid deployment\n"
- fi
+echo "Installing Splunk UF"
+read -s -p "Enter the UF server ip or DNS that this server will connect to: " ufserver
+read -s -p "Enter the port that this server will connect on: " ufport
+read -s -p "Enter the UF username to connect: " ufusername
+read -s -p " and Password: " ufpassword
+# Install and configure the Splunk universal forwarder
+## Install and start the Splunk universal forwarder
+echo Installing and starting the Splunk universal forwarder
+sudo wget -O splunk-8.2.8-da25d08d5d3e-Linux-x86_64.tgz "https://download.splunk.com/products/splunk/releases/8.2.8/linux/splunk-8.2.8-da25d08d5d3e-Linux-x86_64.tgz" &&
+cd /opt && sudo tar -xzf splunk-8* &&
+sudo /opt/splunkforwarder/bin/splunk start --accept-license &&
+## Configure the Splunk universal forwarder
+echo Configuring the Splunk universal forwarder
+sudo /opt/splunkforwarder/bin/splunk enable boot-start &&
+sudo /opt/splunkforwarder/bin/splunk add forward-server $ufserver:$ufport -auth admin:$ufpassword &&
+sudo /opt/splunkforwarder/bin/splunk add monitor /var/log &&
+sudo /opt/splunkforwarder/bin/splunk restart &&
