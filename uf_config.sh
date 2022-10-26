c3ad5d1c05f3731b47706af693ef9b77f3ca5186
deseretsecops <113214308+deseretsecops@users.noreply.github.com>
2022-10-26 11:45:52 -0600

    updated get files for splunk

--- uf_config.sh
+++ uf_config.sh
@@ -1,13 +1,14 @@
+echo "Download and install Splunk UF"
 
+wget -O splunkforwarder-8.2.8-da25d08d5d3e-linux-2.6-x86_64.rpm "https://download.splunk.com/products/universalforwarder/releases/8.2.8/linux/splunkforwarder-8.2.8-da25d08d5d3e-linux-2.6-x86_64.rpm"
+sudo rpm -i splunk-8.2.*
 echo "Configure Splunk UF Script"
 wait 3
 
-read -s " Enter UF Password to Connect: " ufpassword 
-
-sudo /opt/splunkforwarder/bin/splunk start --accept-license &&
+sudo /opt/splunkforwarder/bin/splunk start --accept-license 
 ## Configure the Splunk universal forwarder
 echo Configuring the Splunk universal forwarder
-sudo /opt/splunkforwarder/bin/splunk enable boot-start &&
-sudo /opt/splunkforwarder/bin/splunk add forward-server 64.147.142.237:9989 -auth admin:$ufpassword &&
+sudo /opt/splunkforwarder/bin/splunk enable boot-start 
+sudo /opt/splunkforwarder/bin/splunk add forward-server 64.147.142.237:9989 
 sudo /opt/splunkforwarder/bin/splunk add monitor /var/log &&
 sudo /opt/splunkforwarder/bin/splunk restart &&
