79701d1476a7bccf3eaf3814601b8156d83acc46
deseretsecops <113214308+deseretsecops@users.noreply.github.com>
2022-11-07 16:29:14 -0700

    added deploy server

--- uf_config.sh
+++ uf_config.sh
@@ -9,6 +9,7 @@ sudo /opt/splunkforwarder/bin/splunk start --accept-license
 ## Configure the Splunk universal forwarder
 echo Configuring the Splunk universal forwarder
 sudo /opt/splunkforwarder/bin/splunk enable boot-start 
+sudo /opt/splunkforwarder/bin/splunk add add forward-server 64.147.142.237:9997
 sudo /opt/splunkforwarder/bin/splunk add set deploy-poll 64.147.142.237:8089 
 sudo /opt/splunkforwarder/bin/splunk add monitor /var/log
 sudo /opt/splunkforwarder/bin/splunk restart 
